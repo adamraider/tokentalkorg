b6f0e5135dcb346ef454296afbff8ad2dd5b9d33
Adam Raider <adamjraider@gmail.com>
2016-07-09 16:47:08 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    util = require('gulp-util'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    watch = require('gulp-watch');
+
+var errorHandler = function (error) {
+  console.log(error.message);
+  this.emit("end");
+}
+
+gulp.task('default', ['styles']);
+
+gulp.task('styles', function() {
+  return gulp.src('./src/sass/application.sass')
+    .pipe(plumber({ errorHandler: errorHandler }))
+    .pipe(sass({ indentedSyntax: true }).on('error', util.log))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({ stream:true }))
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  watch('./src/sass/**/*.sass', function(){ gulp.start('styles'); })
+});
